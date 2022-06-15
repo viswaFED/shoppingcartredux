21afdf5e040f32db59cba01d1f40ad7e2dcf16fa
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-06-15 15:29:52 +0530

    cartactivity

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Cart from './components/Cart/Cart';
+import { useSelector } from 'react-redux';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
+
+function App() {
+  const showCart =  useSelector(state=>state.ui.cartIsVisible);
+  return (
+    <Layout>
+      {showCart &&<Cart />}
+      <Products />
+    </Layout>
+  );
+}
+
+export default App;
